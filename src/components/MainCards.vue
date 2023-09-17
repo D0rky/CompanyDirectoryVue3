3fa1a0fd4ff753ed23adbf0c193f1771553fdf28
Adam <martia123456789@gmail.com>
2023-09-17 18:28:55 -0500

    complete

---
+++ src/components/MainCards.vue
@@ -0,0 +1,33 @@
+<script setup>
+import MainCardsSingle from './MainCardsSingle.vue';
+
+</script>
+
+
+
+<template>
+    <div class="sub-wrapper">
+        <MainCardsSingle v-for="n in 20" />
+    </div>
+</template>
+
+
+
+
+<style scoped lang="postcss">
+.sub-wrapper {
+    @apply grid grid-cols-1 gap-4;
+
+    @screen md {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    @screen lg {
+        grid-template-columns: repeat(3, 1fr);
+    }
+
+    @screen xl {
+        grid-template-columns: repeat(4, 1fr);
+    }
+}
+</style>
---
+++ src/components/MainPagination.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+const pages = ref(10)
+const activePage = ref(4)
+
+const prevPage = () => {
+    if (activePage.value > 1) {
+        activePage.value--
+    }
+}
+const nextPage = () => {
+    if (activePage.value < pages.value) {
+        activePage.value++
+    }
+}
+</script>
+
+<template>
+    <div class="pagination">
+        <button class="action" :disabled="activePage === 1" @click="prevPage">Prev</button>
+        <button class="page" v-for="page in pages" :class="page === activePage ? 'active' : ''" @click="activePage = page">
+            {{ page }}
+        </button>
+        <button class="action" :disabled="activePage === pages" @click="nextPage">Next</button>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+.pagination {
+    @apply flex justify-center gap-4;
+
+    .action {
+        @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover: disabled:bg-slate-100;
+    }
+
+    .page {
+        @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:;
+
+        &.active {
+            @apply text-slate-100 hover: bg-yellow-600;
+        }
+    }
+}
+</style>
