028139fd8a24581eaeeb1a5ee44abf0e0b9cab5a
Adam <martia123456789@gmail.com>
2023-09-24 18:17:17 -0500

    Final Commits

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-import NavMenu from '@/components/NavMenu.vue'
-import Main from '@/components/Main.vue';
+
+import NavMenu from '@/components/NavMenu.vue';
+
 </script>
 
 <template>
   <NavMenu />
-  <Main />
+  <RouterView />
 </template>
