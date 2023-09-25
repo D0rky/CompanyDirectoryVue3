9e203204fbac628603cf744cdd51ce1d0ec52295
Adam <martia123456789@gmail.com>
2023-09-24 19:08:08 -0500

    Final

--- src/components/MainCardsSingle.vue
+++ src/components/MainCardsSingle.vue
@@ -1,56 +1,56 @@
 <script setup>
-import * as faker from '@faker-js/faker';
+import { faker } from '@faker-js/faker'
 
-const firstName = faker.name.firstName();
-const lastName = faker.name.lastName();
-const fullName = `${firstName}${lastName}`;
+const firstName = faker.name.firstName()
+const lastName = faker.name.lastName()
+const fullName = `${firstName} ${lastName}`
 
-const selectClick = () => {
+const selectCard = () => {
     console.log(`${fullName} selected`)
 }
 </script>
 
-
-
-
 <template>
-    <div class="card" @click="selectClick">
+    <div class="card" @click="selectCard">
         <div class="card-image">
-            <img :src="faker.internet.avatar()" alt=" " srcset=" " />
+            <img :src="faker.internet.avatar()" alt="" srcset="" />
         </div>
         <div class="card-details">
             <p class="card-details-name">{{ fullName }}</p>
             <p class="card-details-job">{{ faker.name.jobTitle() }}, {{ faker.name.jobArea() }}</p>
-            <p class="card-details-email">{{ faker.internet.exampleEmail(firstName, lastName) }}</p>
-            <p class="card-details-quote">{{ faker.lorem.paragraph() }}</p>
+            <p class="card-details-quote">"{{ faker.lorem.paragraph() }}"</p>
         </div>
-
     </div>
 </template>
 
 <style scoped lang="postcss">
 .card {
-    @apply cursor-pointer overflow-hidden rounded-md bg-slate-50 p-8 shadow-md hover:scale-125 transition-transform duration-300 hover:shadow-slate-900;
+    @apply cursor-pointer overflow-hidden rounded-md bg-slate-100 p-8 shadow-md hover:scale-110 transition-transform duration-500 hover:shadow-slate-700 hover:shadow-2xl;
 
     &-image {
         img {
-            @apply mx-auto rounded-full object-contain;
+            @apply mx-auto rounded-full object-contain
         }
+
     }
 
     &-details {
-        @apply flex flex-col gap-2 pt-6 text-center;
+        @apply flex flex-col pt-6 text-center;
 
         &-name {
-            @apply text-3xl font-thin tracking-wider text-slate-800;
+            @apply text-3xl font-thin tracking-wider text-yellow-800;
         }
 
         &-job {
-            @apply -mt-2 text-xs font-bold text-yellow-700;
+            @apply text-xs text-red-800;
+        }
+
+        &-email {
+            @apply text-xs text-slate-700 font-bold;
         }
 
         &-quote {
-            @apply pt-4 text-lg italic text-slate-800;
+            @apply text-lg pt-4 italic text-slate-700;
         }
     }
 }
